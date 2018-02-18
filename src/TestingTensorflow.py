10e6f0de2bd9629b24817d154591ea272f660e0e
victorcas04 <victorcas95@gmail.com>
2018-02-18 20:32:23 +0100

    Update documentation and tensorflow example successfully tested.

--- src/TestingTensorflow.py
+++ src/TestingTensorflow.py
@@ -1,14 +1,14 @@
 
 import tensorflow as tf
 
-hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
-sess.run(hello)
-###tf.Print()
-print(str(hello))
 
+hello = tf.constant('Hello, TensorFlow!')
 a = tf.constant(10)
 b = tf.constant(32)
-sess.run(a + b)
+
+print()
+print(sess.run(hello))
+print(sess.run(a + b))
 
 sess.close()
