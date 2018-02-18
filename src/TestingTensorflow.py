476d109e398d8cd6128adac30bb5765a3dbdffe6
victorcas04 <victorcas95@gmail.com>
2018-02-18 19:39:21 +0100

    Change .gitignore to avoid merge problems with untracked files (class diagrams and flow charts). Add more documentation.

--- src/TestingTensorflow.py
+++ src/TestingTensorflow.py
@@ -1,4 +1,14 @@
 
 import tensorflow as tf
 
-print("hello world")
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+sess.run(hello)
+###tf.Print()
+print(str(hello))
+
+a = tf.constant(10)
+b = tf.constant(32)
+sess.run(a + b)
+
+sess.close()
