0eb3771fb0f0b3d2e25c1abbdefc6be06fdabbfe
victorcas04 <victorcas95@gmail.com>
2018-02-23 11:49:20 +0100

    Update documentation and tensorflow example successfully tested.

---
+++ src/ImageCaptureFromFile.py
@@ -0,0 +1,20 @@
+
+from PIL import Image
+
+class ImageCaptureFromFileClass(object):
+
+    name = ""
+
+    def __init__(self, name):
+        self.name = name
+
+    def captureImageFromFile(self):
+        img = Image.open(self.name)
+        img.show()
+        return
+        f = open(self.name, 'r+')
+        jpgdata = f.read()
+        print("2")
+        print(jpgdata)
+        f.close()
+
