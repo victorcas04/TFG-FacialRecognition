537dd4c231ef63b0d3ec0c9c41004f909c2b08ed
victorcas04 <victorcas95@gmail.com>
2018-02-23 12:12:43 +0100

    Load image from file by default.

--- src/ImageCaptureFromFile.py
+++ src/ImageCaptureFromFile.py
@@ -6,15 +6,19 @@ class ImageCaptureFromFileClass(object):
     name = ""
 
     def __init__(self, name):
+
         self.name = name
 
     def captureImageFromFile(self):
+
         img = Image.open(self.name)
         img.show()
         return
+
         f = open(self.name, 'r+')
         jpgdata = f.read()
         print("2")
         print(jpgdata)
         f.close()
+        
 
--- src/Main.py
+++ src/Main.py
@@ -2,11 +2,13 @@ from src.ImageCaptureFromFile import ImageCaptureFromFileClass as iFFClass
 
 
 class MainClass(object):
+
     name1 = "..\sources\FOTO_DNI_1.jpg"
     name2 = "..\sources\FOTO_DNI_2.jpg"
     default = "..\sources\default.png"
 
     def loadI(name=default):
+
         iFF = iFFClass(name)
         iFF.captureImageFromFile()
         print("Image loaded successfully")
