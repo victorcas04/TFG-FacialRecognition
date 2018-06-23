caa682e169783f9890daa7480a2043ce16516761
victorcas04 <victorcas95@gmail.com>
2018-06-23 23:56:11 +0200

    Documentation added
    Main.py and Camera.py documentation finished.

--- src/Camera.py
+++ src/Camera.py
@@ -1,95 +1,184 @@
 
 # encoding: utf-8
 
+'''
+@author: victorcas
+'''
+
 import cv2
 import Util as util
 import Files as files
 import TextInterface as txtIf
 
-# max resolution with integrated webcam (sony vaio VPCF13A4E): 640 X 480
-# max resolution with external camera (logitech WEBCAM C170): 1024 X 768
+'''
+The maximum resolution with the integrated webcam (from Sony Vaio VPCF13A4E in our case): [640 X 480]
+The maximum resolution with an external camera (Logitech WEBCAM C170 in our case): [1024 X 768]
+
+If we know the maximum resolution from our camera, edit this values.
+If we don't know it, just ignore this and a default value will be taken ([640 X 480]).
+'''
 resH = 768
 resW = 1024
 
-#  [0] para la webcam integrada en el portatil o una externa en caso de haber
-#  [1] para la webcam integrada en el portatil en caso de que haya una externa conectada
-# [-1] para menú (no recomendado)
+'''
+To choose a camera, just change the camera index below.
+
+Use [0] to use the first external camera installed. If no external cameras are installed, 
+    it will use the integrated camera.
+    
+Use [1] to willfully use the integrated camera while another external camera is installed.
+
+Use [-1] to display a menu with all the cameras installed (integrated and external).
+    Not recommended because of some problems while displaying the image.
+'''
 cameraIdx = 0
+
+'''
+Method that initializes the camera with index=cameraIdx and returns it (if available, returns None otherwise)
+@param None
+@return cap VideoCapture
+    VideoCapture object with the camera initialized or None otherwise
+'''
 
 def initializeCamera():
 
     txtIf.printMessage(txtIf.MESSAGES.INITIALIZING_CAMERA)
 
+    '''
+    Get the camera with given index
+    '''
     cap = cv2.VideoCapture(cameraIdx)
 
+    '''
+    If camera couldn't be open, print error message and return None
+    '''
     if not cap.isOpened():
         txtIf.printError(txtIf.ERRORS.CAMERA_NOT_INITIALIZED)
         return None
 
+    '''
+    If camera could be open, initialize parameters (FPS fixed at 60 to get better user experience)
+    '''
     cap.set(cv2.CAP_PROP_FPS, int(60))
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, resH)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, resW)
 
     txtIf.printMessage(txtIf.MESSAGES.RESOLUTION_CAMERA, [int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))])
-     
-    # Esperamos dos segundos para que la cámara termine de inicializarse y no tomar datos basura
+
+    '''
+    Wait a few seconds to let the camera finish initializing and avoid taking trash data
+    '''
     import time
     time.sleep(2)
+
     return cap
+
+'''
+Method that captures an image to save it to the database or to compare it
+@param captureTocompare boolean
+    If True, the image is returned to be compared
+    If False, no image is returned, but extra information is asked for to save the image in the database 
+@return captured boolean 
+    False if there was any problem, True otherwise
+@return imageToReturn Image
+    Image captured in case we had to return it, None otherwise
+'''
 
 def captureImage(captureToCompare=False):
     import math
     import numpy as np
-    # Para mostrar informacion sobre la version de cv
-    # print(cv2.getBuildInformation())
-
     imageToReturn = None
     captured = False
 
+    '''
+    Get the camera initialized and continue, or return default image
+    '''
     cap = initializeCamera()
     if cap is None:
         return files.loadImage()
 
+    '''
+    Print main options to use with the camera
+    '''
     txtIf.printMessage(txtIf.MESSAGES.IMAGE_IN_REALTIME)
     txtIf.printMenuFaceInBox()
 
+    '''
+    Loop to display image in real-time, so the user knows exactly what frame is getting captured
+    '''
     while True:
         numFaces = 0
+
+        '''
+        Read actual frame
+        '''
         ret, frame = cap.read()
+
+        '''
+        Flip image to display it correctly (otherwise the right side of the image will be our left and viceversa)
+        '''
         frame = cv2.flip(frame, 1)
         frameToShow = np.copy(frame)
 
+        '''
+        Need to convert image to gray so we can work with it
+        '''
         gray = util.imageToGrayscale(frameToShow)
+
+        '''
+        Get all the faces from the image (it's an array of rectangles (x, y, w, h))
+        '''
         faces = util.getFacesMultiScale(gray)
 
+        '''
+        Set parameters to personalize rectangles displayed over the faces (thickness changes automatically depending on
+            the size of the image, and color is blue by default, but can be changed)
+        '''
         rectangleThickness = int((frameToShow.shape[0] + frameToShow.shape[1]) / (100 * 2 * math.pi))  # 20-30
         rectangleColor = (255, 0, 0)
 
+        '''
+        Count the number of faces and set rectangles over the image
+        '''
         for (x, y, w, h) in faces:
             numFaces = numFaces + 1
             cv2.rectangle(frameToShow, (x, y), (x + w, y + h), rectangleColor, rectangleThickness)
 
+        '''
+        Display the image we are recording with rectangles on it for each face
+        '''
         cv2.imshow('Real-time image', frameToShow)
         k = cv2.waitKey(1)
 
-        ### Press [I] for info.
+        '''
+        If [I] is press, some information will be displayed on text interface.
+        '''
         if k == ord('i'):
             if numFaces != 1:
                 txtIf.printError(txtIf.ERRORS.IMAGE_TOO_MANY_FACES, numFaces)
             txtIf.printMenuFaceInBox()
 
-        ### Press [Q] to exit.
+        '''
+        If [Q] is press, exit loop without take any frame.
+        '''
         if k == ord('q'):
             break
 
-        ### Press [P] to pause camera reading.
-        ### Press [SPACE] to resume camera reading.
+        '''
+        If [P] is press, pause camera reading at this point.
+            The camera will remain paused until we press [SPACE]
+            If any other key apart from [SPACE] is press during the pause, a message will be displayed.
+        '''
         if k == ord('p'):
             txtIf.printMessage(txtIf.MESSAGES.CAMERA_PAUSED)
             while cv2.waitKey(0) != ord(' '):
                 txtIf.printMessage(txtIf.MESSAGES.CAMERA_PAUSED_2)
 
-        ### Press [C] to take the actual frame and exit if there is only 1 person.
+        '''
+        If [C] is press, check if the number of faces on the image is one.
+            If that's the case take that frame and exit loop.
+            If more (or none) faces are in the image, a error message will be shown. 
+        '''
         if k == ord('c'):
             if numFaces == 1:
                 imageToReturn = frame
@@ -97,14 +186,27 @@ def captureImage(captureToCompare=False):
             else:
                 txtIf.printError(txtIf.ERRORS.IMAGE_TOO_MANY_FACES, numFaces)
 
+    '''
+    Close the camera and destroy the window created to display the image in real-time.
+    '''
     cap.release()
     cv2.destroyAllWindows()
 
+    '''
+    If an image was taken...
+    '''
     if imageToReturn is not None:
         captured = True
         if captureToCompare is False:
+            '''
+            ...if it was taken to save to database, ask the user for more data. If something goes wrong in
+                that process, 'captured' will be false so any images are saved.
+            '''
             captured = files.doWhenNewImage(imageToReturn)
 
+    '''
+    If no image was taken, and if the purpose of this was to compare it, load the default image.
+    '''
     if imageToReturn is None and captureToCompare is True:
         imageToReturn = files.loadImage()
 
--- src/Main.py
+++ src/Main.py
@@ -1,29 +1,47 @@
 
 # encoding: utf-8
 
+'''
+@author: victorcas
+'''
+
 from __future__ import division
 import Util as util
-import Files as files
 import TextInterface as txtIf
 from GUI import GUIClass as gui
 import Camera as camera
 
-if __name__ == "__main__":
+'''
+Contains the main method of the program execution.
+'''
 
-    delimiter = files.delimiter
-    pathDatasetFullImages = files.datasetPath
-    pathDatasetFacesImages = files.facesDatasetPath
+if __name__ == "__main__":
 
     txtIf.printMessage(txtIf.MESSAGES.TITLE)
 
-    while True:
+    '''
+    Main loop to run the program until the user exits.
+    '''
 
+    while True:
         askTrain = True
+
+        '''
+        Ask the user if wants to add a new image to the database.
+        On loop to avoid run the program for each image to add.
+        '''
+
         while txtIf.askNewImage():
             if camera.captureImage()[0]:
+                '''
+                If we add one image or more, the network will be trained without asking the user. 
+                '''
                 askTrain = False
 
         if (util.askTrain() if askTrain else util.train()):
+            '''
+            Ask the user if wants to recognize someone from file, from a single camera shot or in real-time.
+            '''
             txtIf.printMessage(txtIf.MESSAGES.ASK_CAMERA_FILE_REALTIME)
             c = txtIf.getScan()
 
@@ -31,25 +49,45 @@ if __name__ == "__main__":
             realTime = False
 
             if c.__eq__("C") or c.__eq__("c"):
-                # Sacamos la imágen que queremos identificar desde la cámara
+                '''
+                If the user press [C], take a picture from camera to compare it.
+                If 'captureToCompare' is set to 'False', the photo will be taken to save on the database.
+                '''
                 photoOriginal = camera.captureImage(captureToCompare=True)[1]
 
             elif c.__eq__("X") or c.__eq__("x"):
+                '''
+                If the user press [X], exit this part of the program and go to the end.
+                '''
                 realTime = True
 
             elif c.__eq__("F") or c.__eq__("f"):
-                # Sacamos la imágen que queremos identificar desde un archivo (en modo gráfico)
+                '''
+                If the user press [F], load an image from a file in the system.
+                A new interface window is created, but is destroyed after we take a file, 
+                    so no problems with multiple window objects from tKinter.
+                '''
                 txtIf.printMessage(txtIf.MESSAGES.INITIALIZING_INTERFACE)
                 guiMain = gui.getInstance()
                 photoOriginal = guiMain.loadImageByGUI()
 
             else:
+                '''
+                If we press [R] (or any other button), the real-time recognition process will begin.
+                '''
                 import RecognizerRealTime as rrt
                 rrt.compareInRealTime()
                 realTime = True
 
+            '''
+            If we have pressed [C] or [F], the next process will be done. Pass it otherwise.
+            '''
+
             if not realTime:
-                # Se hace en este órden y no antes porque da problemas con la interfaz nativa de cv
+                '''
+                We have to initialize the main window of the interface here and not before because of some 
+                    compatibility problems between tKinter and OpenCV
+                '''
                 if guiMain is None:
                     txtIf.printMessage(txtIf.MESSAGES.INITIALIZING_INTERFACE)
                     guiMain = gui.getInstance()
@@ -57,10 +95,20 @@ if __name__ == "__main__":
                 guiMain.fixedSize()
                 guiMain.createTop_BottomPanel()
 
+                '''
+                Cut the image to obtain only the face.
+                '''
                 cutted, pOri = util.cutFaceFromImage(photoOriginal)
+
                 if cutted:
+                    '''
+                    If we could cut a face, compare that face and display on the interface.
+                    '''
                     import CompareImages as compareImages
-                    # Imágen resultado / Porcentaje comparación / Nombre imágen resultado / Información sobre la imagen
+
+                    '''
+                    The return of this method gives us the image (i), a percentage (p) and the name of the image (n)
+                    '''
                     i, p, n = compareImages.compare(pOri)
 
                     guiMain.setTitleAndProgress(p, n)
@@ -68,11 +116,17 @@ if __name__ == "__main__":
                     guiMain.setImage(image=i, left=False)
 
                 else:
+                    '''
+                    If a face cannot be cut (too many faces or not at all), display default image.
+                    '''
                     guiMain.setTitleAndProgress()
                     guiMain.setImage(image=photoOriginal)
                     guiMain.setImage(left=False)
 
                 guiMain.displayWindow()
 
+        '''
+        Ask the user if wants to continue again with same process or to exit the application.
+        '''
         if txtIf.askExitMain():
             break
